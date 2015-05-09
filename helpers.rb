45e375ee01c5b7b9fdb2140d35cdb757602c105c
Grace Z <gz.gracez16@gmail.com>
2015-05-09 00:03:33 -0400

    findTicker, averageReturn based on ticker

--- helpers.rb
+++ helpers.rb
@@ -10,3 +10,27 @@ require 'json'
 
 require 'dm-core'
 require 'dm-migrations'
+require './stocks'
+require './env' if development?
+
+helpers do
+	#Get data as JSON
+	def findTicker(ticker)
+		@key=ENV['key']
+		uri = URI.parse("https://www.quandl.com/api/v1/datasets/#{ticker}/AAPL.json?trim_start=2014-05-01&auth_token=#@key")
+		response = Net::HTTP.get_response(uri)
+		responseBody = response.body
+		parsed=JSON.parse(responseBody)
+		return parsed
+		#@average=averageReturn(parsed).to_s
+	end
+
+	#Get average return
+	def averageReturn(ticker)
+		total=0
+		for i in ticker["data"]
+			total+=i[3].to_f
+		end
+		return (total/ticker["data"].length).round(2)
+	end
+end
--- main.rb
+++ main.rb
@@ -16,28 +16,10 @@ configure :production do
   DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
 
-helpers do
-  def averageReturn(ticker)
-    total=0
-    for i in ticker["data"]
-      total+=i[3].to_f
-      #ticker["data"][0][3].to_s
-    end
-    #puts total/ticker["data"].length
-    return (total/ticker["data"].length).round(2)
-  end
-end
-
 get '/api' do
-  @key=ENV['key']
   @title="Temp, API, APPL"
-  uri = URI.parse("https://www.quandl.com/api/v1/datasets/WIKI/AAPL.json?trim_start=2014-05-01&auth_token=#@key")
-
-  # Shortcut
-  response = Net::HTTP.get_response(uri)
-  responseBody = response.body
-  parsed=JSON.parse(responseBody)
-  @average=averageReturn(parsed).to_s
+  tickerJSON=findTicker("APPL")
+  @average=averageReturn(tickerJSON)
   erb :api
 end
 
