2d5de6350b20a1ee37c7eec64dded4f8c1b374ba
Grace Z <gz.gracez16@gmail.com>
2015-05-15 20:41:28 -0400

    merge to home

--- main.rb
+++ main.rb
@@ -28,11 +28,6 @@ get '/' do
   erb :home
 end
 
-get '/home' do
-  @title = "InvestI's Home Page!"
-  erb :homeT, :layout => false
-end
-
 get '/test' do
   @title="InvestI's Test Page"
   test="test"
