3ae807da94c9367d66d61b994b7eddc483422f05
Grace Z <gz.gracez16@gmail.com>
2015-06-03 19:24:08 -0400

    users_table

--- main.rb
+++ main.rb
@@ -6,62 +6,67 @@ require './users'
 require './env' if development?
 
 #get('/styles.css'){ scss :styles, :syntax => :scss, :style => :compressed }
+# class Application < Sinatra::Application
+    enable :sessions
 
-enable :sessions
+    def initialize
+      # @users_table = DB[:users]
+      @users_table = Users.all
+      # puts @users_table
+    end
+    
+    # def initialize(app=nil)
+    #   super(app)
+    #   # @users_table = DB[:users]
+    #   @users_table = Users.all
+    #   # puts @users_table
+    # end
 
-  def initialize
-    # @users_table = DB[:users]
-    @users_table = Users.all
-    puts @users_table
-  end
+    configure :development do
+      DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+    end
 
-configure :development do
-  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
-end
+    configure :production do
+      DataMapper.setup(:default, ENV['DATABASE_URL'])
+    end
 
-configure :production do
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
-end
+    get '/api' do
+      @title="Temp, API, AAPL"
+      tickerJSON=findTicker("AAPL")
+      erb :api
+    end
 
-get '/api' do
-  @title="Temp, API, AAPL"
-  tickerJSON=findTicker("AAPL")
-  # @average=averageReturn(tickerJSON)
-  erb :api
-end
+    get '/' do
+      @title = "InvestI's Home Page!"
+      puts @users_table
+      erb :home
+    end
 
-get '/' do
-  @title = "InvestI's Home Page!"
-  # @user
-  # @login=false
-  erb :home
-end
+    get '/test/?' do
+      @title="InvestI's Test Page"
+      test="test"
+      "Hello #{test}"
+      erb :test
+    end
 
-get '/test/?' do
-  @title="InvestI's Test Page"
-  test="test"
-  "Hello #{test}"
-  erb :test
-end
+    get '/reload' do
+      @title="InvestI's Reload Page"
+      test="reload"
+      "Hello #{test}"
+      erb :reload
+    end
 
-get '/reload' do
-  @title="InvestI's Reload Page"
-  test="reload"
-  "Hello #{test}"
-  erb :reload
-end
+    not_found do
+      @title="404 Error"
+      erb :not_found, :layout => false
+    end
 
-not_found do
-  @title="404 Error"
-  erb :not_found, :layout => false
-end
-
-helpers do
-  def current?(path='/')
-    (request.path==path || request.path==path+'/') ? "current" : nil
-  end
-end
-
+    helpers do
+      def current?(path='/')
+        (request.path==path || request.path==path+'/') ? "current" : nil
+      end
+    end
+# end
 
 
 #get '/:name' do
