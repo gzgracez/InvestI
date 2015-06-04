6ca1261cdc7acfb73245037f22a2fb24e10a0a7e
Grace Z <gz.gracez16@gmail.com>
2015-06-04 12:19:39 -0400

    account info page

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # InvestI
-To run locally, create a `file.rb` file that contains: 
+To run locally, create a `env.rb` file that contains: 
 ```
 require './helpers'
-$key="INSERT_KEY_HERE"
+ENV['key']="INSERT_KEY_HERE"
 ```
--- users.rb
+++ users.rb
@@ -56,6 +56,42 @@ get '/users/?' do
   end
 end
 
+get '/account/?' do 
+  @title = "Account Information"
+  user = findUserInDB(session[:id])
+  if user
+    @firstName = user[:firstName]
+    @lastName = user[:lastName]
+    @username = user[:username]
+    @email = user[:email]
+    puts @firstName
+    erb :accountInfo
+  else
+    erb :notLoggedIn
+  end
+end
+
+# get '/account/edit/?' do
+#   @title = "Edit " + Users.get(session[:id]).username
+#   @user=Users.get(session[:id])
+#   erb :edit_task
+# end
+
+# put '/user/:id' do
+#   @title = "Update " + Users.get(params[:id]).name
+#   tasks=find_task
+#   if tasks.update(params[:tasks])
+#     flash[:notice] = "Task successfully updated"
+#   end
+#   redirect to("/tasks/#{tasks.id}")
+# end
+
+# delete '/user/:id' do
+#   @title = "Delete " + Users.get(params[:id]).name
+#   find_task.destroy
+#   redirect to("/tasks")
+# end
+
 # get '/user/:id' do
 #   @title = Users.get(params[:id]).firstName.capitalize + " " + Users.get(params[:id]).lastName.capitalize + "\'s Account!"
 #   @user = find_user
